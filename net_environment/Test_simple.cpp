b59cdc7386dca26307d656e42c45292221410395
trobicho <tomtomy35@gmail.com>
2019-08-12 02:17:32 +0200

    WIP

--- net_environment/Test_simple.cpp
+++ net_environment/Test_simple.cpp
@@ -6,7 +6,7 @@
 /*   By: trobicho <trobicho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/09 18:10:18 by trobicho          #+#    #+#             */
-/*   Updated: 2019/08/11 17:40:51 by trobicho         ###   ########.fr       */
+/*   Updated: 2019/08/11 22:55:29 by trobicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,15 @@ Test_simple::Test_simple(int max): m_max(max)
 int	Test_simple::do_evalutation(People_net &people, int generation)
 {
 	int	step;
+	int	score = 0;
 	int	r;
+	std::vector<int>	grid;
 
+	grid.resize(m_max * m_max, 0);
 	m_pos.x = m_max / 2;
 	m_pos.y = m_max / 2;
-	for (step = 0; step < 1000; step++)
+	r = 0;
+	for (step = 0; step < 2600; step++)
 	{
 		m_sensor[0] = m_pos.x / (double)m_max;
 		m_sensor[1] = m_pos.y / (double)m_max;
@@ -47,9 +51,14 @@ int	Test_simple::do_evalutation(People_net &people, int generation)
 		}
 		if (m_pos.x < 0 || m_pos.x >= m_max || m_pos.y < 0 || m_pos.y >= m_max)
 			break;
+		if (grid[m_pos.x + m_pos.y * m_max] == 0)
+			score++;
+		else
+			score--;
+		grid[m_pos.x + m_pos.y * m_max]++;
 	}
-	people.set_score(step);
-	return (step);
+	people.set_score(score);
+	return (score);
 }
 
 
